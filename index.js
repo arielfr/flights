e10fe10a2c04e94393e41053e25cfa18f6621ba3
ARIEL REY <ariel.rey@mercadolibre.com>
2017-07-12 13:15:47 -0300

    Adding timeout on notification and array to dont duplicate the notifications

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ const schedule = require('node-schedule');
 // Office exact position
 const officePosition = new Position(-34.5277426, -58.4687091);
 
+// Save the flights already showed
+let flightsNotified = [];
+
 // Run a schedule every 30 seconds
 schedule.scheduleJob('*/30 * * * * *', function () {
   flightIsNearTheOffice().catch(error => {
     console.log('An error ocurred: ' + error);
   });
+});
+
+// Clean the flights notified every 5 minutes
+schedule.scheduleJob('*/5 * * * *', function () {
+  flightsNotified = [];
 });
 
 /**
@@ -30,12 +38,16 @@ function flightIsNearTheOffice() {
         if (flight.latitude !== null &&
           flight.longitude !== null &&
           Location.isInsideRadius(flight.getPrettyPosition(), officePosition.getPosition(), 3000) &&
-          Location.isGoingInDirection(flight.getPrettyPosition(), officePosition.getPosition(), 'E')) {
+          Location.isGoingInDirection(flight.getPrettyPosition(), officePosition.getPosition(), 'E') &&
+          flightsNotified.indexOf(flight.callsign) === -1) {
           // Send a system notification
           notifier.notify({
             title: 'Avión aproximandose',
-            message: `Avión ${flight.callsign} proveniente de ${flight.originCountry} a ${flight.getSpeedOnKilometers()}km/h`
+            message: `Avión ${flight.callsign} proveniente de ${flight.originCountry} a ${flight.getSpeedOnKilometers()}km/h`,
+            timeout: 15
           });
+
+          flightsNotified.push(flight.callsign);
         }
       });
 
