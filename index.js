85835364390085177f5e5b61facbb1294f21c2c2
Ariel Rey <ariel.rey@mercadolibre.com>
2020-02-10 12:10:40 -0300

    Updating time

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ schedule.scheduleJob('*/30 * * * * *', function () {
 });
 
 // Clean the flights notified every 5 minutes
-schedule.scheduleJob('*/5 * * * *', function () {
+schedule.scheduleJob('*/1 * * * *', function () {
   flightsNotified = [];
 });
 
